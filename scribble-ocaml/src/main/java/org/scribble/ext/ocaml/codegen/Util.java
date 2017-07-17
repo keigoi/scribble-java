23fdf7dfeca508f3ae5711e7e0cba41afb2ead5d
Keigo IMAI <keigo.imai@gmail.com>
2017-07-18 00:21:49 +0900

    feature: in-place generation of delegated session types

--- scribble-ocaml/src/main/java/org/scribble/ext/ocaml/codegen/Util.java
+++ scribble-ocaml/src/main/java/org/scribble/ext/ocaml/codegen/Util.java
@@ -4,10 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiFunction;
 
+import org.scribble.ast.Module;
+import org.scribble.ast.global.GProtocolDecl;
+import org.scribble.main.ScribbleException;
 import org.scribble.model.MAction;
 import org.scribble.model.MState;
 import org.scribble.sesstype.kind.ProtocolKind;
+import org.scribble.sesstype.name.GProtocolName;
+import org.scribble.sesstype.name.LProtocolName;
 import org.scribble.sesstype.name.MessageId;
+import org.scribble.sesstype.name.Role;
 
 public class Util {
 
@@ -55,4 +61,29 @@ public class Util {
 		}
 		return accum;
 	}
+	
+	public static class GProtocolNameRole {
+		public final GProtocolName name;
+		public final Role role;
+		public GProtocolNameRole(GProtocolName name, Role role) {
+			this.name = name;
+			this.role = role;
+		}
+	}
+	
+	// brain-dead approach to recover global name from a local name
+	public static GProtocolNameRole getGlobalNameAndRole(Module module, LProtocolName local) throws ScribbleException {
+		String localname = local.getSimpleName().toString();
+		for (GProtocolDecl global : module.getGlobalProtocolDecls()) {
+			GProtocolName name = global.getHeader().getDeclName();
+			String globalname = name.getSimpleName().toString();
+			for (Role role : global.header.roledecls.getRoles()) {
+				if (localname.equals(globalname + "_" + role.toString())) {
+					return new GProtocolNameRole(new GProtocolName(module.getFullModuleName(), name), role);
+				}
+			}
+		}
+		throw new RuntimeException("[OCaml] cannot find GProtocolName from LProtocolName " + local);
+	}
+
 }
